7dcbfdb38692f2151e919e88b6d79c549786d0d4
Neelanshi <neelanshibhatnagar@ymail.com>
2019-09-02 12:02:49 +0530

    Final csv file generation

--- movieData-populator-service/src/main/java/com/stackroute/service/MoviePopulatorServiceImpl.java
+++ movieData-populator-service/src/main/java/com/stackroute/service/MoviePopulatorServiceImpl.java
@@ -26,27 +26,22 @@ public class MoviePopulatorServiceImpl implements MoviePopulatorService{
     @Override
 
     public void fetchDataFromOmdbApi() {
-//
         ResponseEntity<String> result;
         String jsonString = "";
         JSONArray jsonArray = new JSONArray();
-        for(int i=501;i<=520;i++) {
+        JSONArray finaljsonArray = new JSONArray();
+        for(int i=100;i<=500;i++) {
 
 
             final String uri = "http://www.omdbapi.com/?apikey=43866b21&i=tt0048" + i;
 
             RestTemplate restTemplate = new RestTemplate();
-            // ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
             result = restTemplate.getForEntity(uri, String.class);
-
-//            System.out.println(result.getBody());
-
             jsonString = result.getBody();
             JSONObject output;
 
             try {
                 output = new JSONObject(jsonString);
-//                System.out.println(output);
                 jsonArray.put(output);
 
             } catch (JSONException e) {
@@ -55,32 +50,37 @@ public class MoviePopulatorServiceImpl implements MoviePopulatorService{
         }
 
         System.out.println(jsonArray);
+        //Iterate through the elements of the array i.
+        //Get thier value.
+//        Removing rating array from jsonarray
         for(int i = 0; i < jsonArray.length(); i++)
         {
             try {
                 JSONObject objects = jsonArray.getJSONObject(i);
-//                String a = objects.get("Title");
+                System.out.println("here");
+                objects.remove("Ratings");
+                finaljsonArray.put(objects);
 
             } catch (JSONException e) {
                 e.printStackTrace();
             }
 
+            //Get the value for the first element and the value for the last element.
         }
-            try {
-                File file = new File("./moviedata.csv");
-                String csv = CDL.toString(jsonArray);
-                FileUtils.writeStringToFile(file, csv);
-                System.out.println("Data has been Sucessfully Writeen to " + file);
-            }  catch (JSONException e) {
-                e.printStackTrace();
-            }
-            catch (IOException e) {
+        System.out.println(finaljsonArray);
+// Generating the csv file from json data cpming from imdb database.
+        try {
+            File file = new File("./moviedata.csv");
+            String csv = CDL.toString(finaljsonArray);
+            FileUtils.writeStringToFile(file, csv);
+            System.out.println("Data has been Sucessfully Writeen to " + file);
+        }  catch (JSONException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
 //                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-
-
-
+            e.printStackTrace();
+        }
 
     }
 }
